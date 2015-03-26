ef3e85cd2db23223d3d7d3f560337d8e7c34777e
Marvin043 <marvin.ztrubas@gmail.com>
2015-03-26 16:12:41 +0100

    Tried to fix maven (failed) and added meeting docs from the monday meeting

--- src/main/java/edu/chl/LifeOfAGoblin/Main.java
+++ src/main/java/edu/chl/LifeOfAGoblin/Main.java
@@ -1,28 +1,15 @@
 package edu.chl.LifeOfAGoblin;
 
-import edu.chl.LifeOfAGoblin.controller.ProjectController;
-import edu.chl.LifeOfAGoblin.model.Project;
-import edu.chl.LifeOfAGoblin.view.ProjectView;
-import com.jme3.app.SimpleApplication;
 
 
 /*
   Application entry class (if using standard java and Swing)
 */
-public class Main extends SimpleApplication {
+public class Main {
 
 	public static void main(String[] args) {
-//		SwingUtilities.invokeLater(() -> {
-                    final Project project = new Project();
-                    final ProjectView projectView = new ProjectView(project);
-                    
-                    ProjectController.create(project, projectView);
-                    projectView.setVisible(true);
-//                });
+            MainScene app = new MainScene();
+            app.start();
 	}
 
-    @Override
-    public void simpleInitApp() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
---
+++ src/main/java/edu/chl/LifeOfAGoblin/MainScene.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin;
+
+import com.jme3.app.SimpleApplication;
+
+
+/**
+ *
+ * @author Anton
+ */
+public class MainScene extends SimpleApplication{
+
+    @Override
+    public void simpleInitApp() {
+        
+    }
+    
+    
+}
