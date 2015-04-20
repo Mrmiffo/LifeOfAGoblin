c09a02feb1e708dd49f2f54b111e3bf2c2cf2dd2
Marvin043 <marvin.ztrubas@gmail.com>
2015-04-20 20:34:43 +0200

    Initialized the main class to start the program

--- src/main/java/edu/chl/LifeOfAGoblin/main/Main.java
+++ src/main/java/edu/chl/LifeOfAGoblin/main/Main.java
@@ -4,10 +4,15 @@
  */
 package edu.chl.LifeOfAGoblin.main;
 
+import edu.chl.LifeOfAGoblin.view.LifeOfAGoblin;
+
 /**
  *
  * @author Anton
  */
 public class Main {
-    
+    public static void main(String[] args){
+        LifeOfAGoblin game = new LifeOfAGoblin();
+        game.start();
+    }
 }
