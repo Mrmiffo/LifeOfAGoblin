1e64bda9e278373bbd6b8ebadfbc7305a94315b0
Marvin043 <marvin.ztrubas@gmail.com>
2015-04-22 14:15:11 +0200

    Added actions

--- src/main/java/edu/chl/LifeOfAGoblin/controller/PlayerListener.java
+++ src/main/java/edu/chl/LifeOfAGoblin/controller/PlayerListener.java
@@ -4,7 +4,9 @@
  */
 package edu.chl.LifeOfAGoblin.controller;
 
+import com.jme3.bullet.control.BetterCharacterControl;
 import com.jme3.input.controls.ActionListener;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.control.AbstractControl;
@@ -17,7 +19,13 @@ public class PlayerListener extends AbstractControl implements ActionListener{
 
     @Override
     public void onAction(String name, boolean isPressed, float tpf) {
-        
+        if (name.equals("walkLeft")){
+            spatial.getControl(BetterCharacterControl.class).setWalkDirection(new Vector3f(-1f, 0f, 0f));
+        } else if (name.equals("walkRight")){
+            spatial.getControl(BetterCharacterControl.class).setWalkDirection(new Vector3f(1f, 0f, 0f));
+        } else if (name.equals("jump") && isPressed){
+            spatial.getControl(BetterCharacterControl.class).jump();
+        }
     }
 
     @Override
