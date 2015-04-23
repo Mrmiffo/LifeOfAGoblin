9951379b28fbebb2423aa5d5d89d0d808441a01a
Marvin043 <marvin.ztrubas@gmail.com>
2015-04-23 08:31:37 +0200

    Cleanup and fixed FaceFront

--- src/main/java/edu/chl/LifeOfAGoblin/controller/PlayerListener.java
+++ src/main/java/edu/chl/LifeOfAGoblin/controller/PlayerListener.java
@@ -1,16 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.chl.LifeOfAGoblin.controller;
 
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.input.controls.ActionListener;
-import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 
 /**
@@ -19,7 +15,7 @@ import com.jme3.scene.control.AbstractControl;
  */
 public class PlayerListener extends AbstractControl implements ActionListener{
     
-    //private CharacterControl playerControl = spatial.getControl(CharacterControl.class);
+    private CharacterControl playerControl;
 
     private String  currentDirection;
     @Override
@@ -39,34 +35,11 @@ public class PlayerListener extends AbstractControl implements ActionListener{
         } else if (name.equals(currentDirection)){
             haltPlayer();  
         }
-        
-//        if (name.equals("walkLeft")){
-//            if (isPressed) {
-//                faceLeft();
-//                spatial.getControl(CharacterControl.class).setWalkDirection(new Vector3f(-0.1f, 0f, 0f));
-//            } else if (!isPressed) {
-//                haltPlayer();
-//            }
-//        }
-//        
-//        if (name.equals("walkRight")){
-//            if (isPressed) {
-//                faceRight();
-//                spatial.getControl(CharacterControl.class).setWalkDirection(new Vector3f(0.1f, 0f, 0f));
-//            } else if (!isPressed) {
-//                haltPlayer();
-//            }
-//        }
-        
-//        if (name.equals("jump") && isPressed){
-//            //TODO implement double jump
-//            spatial.getControl(CharacterControl.class).jump();
-//        }
     }
 
     @Override
     protected void controlUpdate(float tpf) {
-        
+
     }
 
     @Override
@@ -76,26 +49,30 @@ public class PlayerListener extends AbstractControl implements ActionListener{
     
     private void faceLeft() {
         spatial.getControl(CharacterControl.class).setViewDirection(new Vector3f(-1, 0, 0));
-        /*Quaternion turnLeft = new Quaternion();
-        turnLeft.fromAngleAxis(-FastMath.PI/2, new Vector3f(0, 1, 0));
-        spatial.setLocalRotation(turnLeft);*/
     }
     
     private void faceRight() {
         spatial.getControl(CharacterControl.class).setViewDirection(new Vector3f(1, 0, 0));
-        /*Quaternion turnRight = new Quaternion();
-        turnRight.fromAngleAxis(FastMath.PI/2, new Vector3f(0, 1, 0));
-        spatial.setLocalRotation(turnRight);*/
     }
     
     private  void faceFront() {
-        Quaternion turnFront = new Quaternion();
-        turnFront.fromAngleAxis(0, new Vector3f(0, 1, 0));
-        spatial.setLocalRotation(turnFront);
+        spatial.getControl(CharacterControl.class).setViewDirection(new Vector3f(0, 0, 1));
     }
     
     private void haltPlayer() {
         spatial.getControl(CharacterControl.class).setWalkDirection(Vector3f.ZERO);
     }
+    
+    @Override
+    public void setSpatial(Spatial spatial) {
+        
+        super.setSpatial(spatial);
+        
+        if (spatial != null) {
+            playerControl = spatial.getControl(CharacterControl.class);
+        } else {
+            playerControl = null;
+        }
+    } 
     
 }
