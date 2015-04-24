e597db6531baf3a78a2b782878e0859269d8dbb2
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-04-24 13:07:55 +0200

    added default settings

--- src/main/java/edu/chl/LifeOfAGoblin/utils/KeyBind.java
+++ src/main/java/edu/chl/LifeOfAGoblin/utils/KeyBind.java
@@ -5,7 +5,11 @@
 package edu.chl.LifeOfAGoblin.utils;
 
 import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
 import com.jme3.input.controls.KeyTrigger;
+import edu.chl.LifeOfAGoblin.controller.PlayerMoveControl;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,11 +21,13 @@ public class KeyBind {
     private static KeyBind instance;
     private Map<String, Set<KeyTrigger>> keySettings;
     private InputManager inputManager;  //How get?
-
     
-    private KeyBind(){
-        removeDefaultMappings();
+    private KeyBind() {
+        removeDefaultMappings(); //Remove JMonkeys Defaults
+        keySettings = getDefaultKeySettings(); //Start game with defaults
+        //TODO replace with data from settings
         
+        //add keybinding mappings to game
         Set<String> actions = keySettings.keySet();
         for (String action : actions) {
             Set<KeyTrigger> triggers = keySettings.get(action);
@@ -29,6 +35,9 @@ public class KeyBind {
                 inputManager.addMapping(action, trigger);
             }
         }
+        
+        //add keylisteners to game
+        inputManager.addListener(new PlayerMoveControl(), "walkRight", "walkLeft", "jump");
     }
 
     public static synchronized KeyBind getInstance(){
@@ -42,5 +51,26 @@ public class KeyBind {
         inputManager.deleteMapping("INPUT_MAPPING_HIDE_STATS");
         inputManager.deleteMapping("INPUT_MAPPING_CAMERA_POS");
         inputManager.deleteMapping("INPUT_MAPPING_MEMORY");
+        //TODO delete more
+    }
+    
+    private Map<String, Set<KeyTrigger>> getDefaultKeySettings() {
+        Map<String, Set<KeyTrigger>> defaults = new HashMap<>();
+        defaults.put("walkRight", makeSet(KeyInput.KEY_D));
+        defaults.put("walkLeft", makeSet(KeyInput.KEY_A));
+        defaults.put("jump", makeSet(KeyInput.KEY_W, KeyInput.KEY_SPACE));
+        return defaults;
+    }
+    
+    private Set<KeyTrigger> makeSet(int... keys) {
+        if (keys.length == 0) {
+            return null;
+        } else {
+            Set<KeyTrigger> temp = new HashSet<>();
+            for (int key : keys) {
+                temp.add(new KeyTrigger(key));
+            }
+            return temp;
+        }
     }
 }
