57e359815c2888de33572d91900e016a87e36c7c
Marvin043 <marvin.ztrubas@gmail.com>
2015-04-27 22:19:12 +0200

    Implemented the input manager wrapper used to register actionlisteners

--- src/main/java/edu/chl/LifeOfAGoblin/utils/InputManagerWrapper.java
+++ src/main/java/edu/chl/LifeOfAGoblin/utils/InputManagerWrapper.java
@@ -4,10 +4,35 @@
  */
 package edu.chl.LifeOfAGoblin.utils;
 
+import com.jme3.input.InputManager;
+import edu.chl.LifeOfAGoblin.controller.interfaces.IKeyListener;
+
 /**
- *
+ * This is a wrapper clas for the inputManager intended to move actionlistener
+ * registration out of the LifeOfAGoblin class.
  * @author Anton
  */
 public class InputManagerWrapper {
+    private static InputManagerWrapper instance;
+    private static InputManager im;
+    private InputManagerWrapper(){
+
+    }
     
+    public static synchronized InputManagerWrapper getInstance(){
+        if (instance == null){
+            instance = new InputManagerWrapper();
+        }
+        return instance;
+    }
+    
+    public static void initialize(InputManager inputManager){
+        im = inputManager;
+    }
+    
+    public void registerListener(IKeyListener actionListener){
+        for (KeyBindings.KeyBind keyBind : actionListener.getKeyBinds()){
+            im.addListener(actionListener, keyBind.getKeyText());
+        }
+    }
 }
