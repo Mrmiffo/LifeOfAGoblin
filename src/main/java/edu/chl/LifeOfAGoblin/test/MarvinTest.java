db459e32efe021f6e57de7cf56d5308104de0f4e
Marvin043 <marvin.ztrubas@gmail.com>
2015-04-27 22:12:05 +0200

    Modified testclass to work with the new functionality

--- src/main/java/edu/chl/LifeOfAGoblin/test/MarvinTest.java
+++ src/main/java/edu/chl/LifeOfAGoblin/test/MarvinTest.java
@@ -1,28 +1,9 @@
 package edu.chl.LifeOfAGoblin.test;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.asset.TextureKey;
-import com.jme3.asset.plugins.FileLocator;
-import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.collision.shapes.CollisionShape;
-import com.jme3.bullet.control.CharacterControl;
-import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.bullet.util.CollisionShapeFactory;
-import com.jme3.input.controls.KeyTrigger;
-import com.jme3.light.AmbientLight;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
-import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
-import com.jme3.texture.Texture;
-import edu.chl.LifeOfAGoblin.controller.PlayerMoveControl;
-import edu.chl.LifeOfAGoblin.model.Player;
 import edu.chl.LifeOfAGoblin.state.GameAppState;
-import edu.chl.LifeOfAGoblin.utils.Resources;
+import edu.chl.LifeOfAGoblin.utils.InputManagerWrapper;
+import edu.chl.LifeOfAGoblin.utils.StateManagerWrapper;
 
 /**
  * This class is for testing purposes only. It will recreate parts of the application in order to test new functionality.
@@ -42,7 +23,9 @@ public class MarvinTest extends SimpleApplication{
     public void simpleInitApp() {
         //Camera
 //        this.flyCam.setEnabled(false);
-        GameAppState playGame = new GameAppState("testScene");
+        InputManagerWrapper.getInstance().initialize(inputManager);
+        StateManagerWrapper.getInstance().initialize(stateManager);
+        GameAppState playGame = new GameAppState();
         stateManager.attach(playGame);
 
     }
