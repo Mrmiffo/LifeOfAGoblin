50a7af00128d9ea4e1d0f5ae9bd5b40f440f8806
Tandl-os <freb@student.chalmers.se>
2015-04-28 23:43:31 +0200

    Preparations for implementation of checkpoint class

--- src/main/java/edu/chl/LifeOfAGoblin/model/abstractClass/AbstractInanimateObject.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/abstractClass/AbstractInanimateObject.java
@@ -5,17 +5,38 @@
 package edu.chl.LifeOfAGoblin.model.abstractClass;
 
 import com.jme3.scene.Node;
+import edu.chl.LifeOfAGoblin.model.interfaces.IModeledNode;
+import edu.chl.LifeOfAGoblin.model.interfaces.INode;
+import edu.chl.LifeOfAGoblin.utils.NodeFactory;
 import edu.chl.LifeOfAGoblin.utils.Resources;
+import java.util.Map;
 
 
 /**
  *
  * @author Anton
  */
-public abstract class AbstractInanimateObject {
+public abstract class AbstractInanimateObject implements INode, IModeledNode {
     protected Node object;
+    protected String model;
     
    protected AbstractInanimateObject(String modelName) {
        object = (Node) Resources.getInstance().getResources(modelName);
+       this.model = modelName;
    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public Map<String, Object> getNodeData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public NodeFactory.NodeType getNodeType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
