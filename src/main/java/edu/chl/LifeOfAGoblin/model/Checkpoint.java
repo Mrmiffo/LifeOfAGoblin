6dce9d2f0bc6b876d6dcc145845a72aa5ea44e8e
Tandl-os <freb@student.chalmers.se>
2015-04-29 20:58:10 +0200

    Finished checkpoint class for now

--- src/main/java/edu/chl/LifeOfAGoblin/model/Checkpoint.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/Checkpoint.java
@@ -4,7 +4,11 @@
  */
 package edu.chl.LifeOfAGoblin.model;
 
+import com.jme3.math.Vector3f;
 import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractInanimateObject;
+import edu.chl.LifeOfAGoblin.utils.NodeFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -13,11 +17,36 @@ import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractInanimateObject;
  * progress class and that the player can respawn at.
  */
 public class Checkpoint extends AbstractInanimateObject {
+    private int level;
+    private int number;
+    private int height;
+    private int width;
+    private int depth;
     
-    public Checkpoint(){
-        super("Checkpoint");
+    public Checkpoint(int level, int number, int height, int width, int depth){
+        super();
         object.setUserData("objectType", "Checkpoint");
+        this.level = level;
+        this.number = number;
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
     }
     
+    @Override
+    public NodeFactory.NodeType getNodeType() {
+        return NodeFactory.NodeType.CHECKPOINT;
+    }
+    
+        @Override
+    public Map<String, Object> getNodeData() {
+        Map<String, Object> nodeData = new HashMap<>();
+        nodeData.put("level", level);
+        nodeData.put("number", number);
+        nodeData.put("width", width);
+        nodeData.put("height", height);
+        nodeData.put("depth", depth);        
+        return nodeData;
+    }
      
 }
