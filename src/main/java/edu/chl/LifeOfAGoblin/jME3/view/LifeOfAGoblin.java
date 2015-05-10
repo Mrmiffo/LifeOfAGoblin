d6fe74c62a02d083d8c659514a1c22eff44b0520
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 20:19:37 +0200

    Added initializers

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/view/LifeOfAGoblin.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/view/LifeOfAGoblin.java
@@ -8,7 +8,9 @@ import com.jme3.app.SimpleApplication;
 import edu.chl.LifeOfAGoblin.jME3.view.state.GameAppState;
 import edu.chl.LifeOfAGoblin.jME3.utils.InputManagerWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.NiftyGUIWrapper;
+import edu.chl.LifeOfAGoblin.jME3.utils.Resources;
 import edu.chl.LifeOfAGoblin.jME3.utils.StateManagerWrapper;
+import edu.chl.LifeOfAGoblin.jME3.view.niftyScreen.MainMenu;
 
 
 /**
@@ -19,14 +21,15 @@ public class LifeOfAGoblin extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        NiftyGUIWrapper.getInstance().initialize(this);
         InputManagerWrapper.getInstance().initialize(inputManager);
         StateManagerWrapper.getInstance().initialize(stateManager);
-        NiftyGUIWrapper.getInstance().initialize(this);
+        Resources.getInstance().initialize(assetManager);
         
         //Start the game
         //TODO Replace with main menu.
-        GameAppState gameState = new GameAppState();
-        StateManagerWrapper.getInstance().addState(gameState);
+        MainMenu mainMenu = new MainMenu();
+        NiftyGUIWrapper.getInstance().addScreen(mainMenu.getScreenName(), mainMenu.getScreen());
     }
     
 }
