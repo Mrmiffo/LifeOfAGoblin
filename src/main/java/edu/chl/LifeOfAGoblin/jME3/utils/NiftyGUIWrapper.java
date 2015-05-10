ee6316e5ec1997ec34218c68d71bbc3da36801a7
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 20:15:08 +0200

    Moved nifty from main menu to wrapper

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/utils/NiftyGUIWrapper.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/utils/NiftyGUIWrapper.java
@@ -7,6 +7,7 @@ package edu.chl.LifeOfAGoblin.jME3.utils;
 import com.jme3.app.Application;
 import com.jme3.niftygui.NiftyJmeDisplay;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
 
 /**
  *
@@ -31,5 +32,29 @@ public class NiftyGUIWrapper {
                 app.getAssetManager(), app.getInputManager(), app.getAudioRenderer(), app.getGuiViewPort());
         this.nifty = niftyDisplay.getNifty();
         app.getGuiViewPort().addProcessor(niftyDisplay);
+    }
+    
+    public void setDebug(Boolean debugStatus){
+        nifty.setDebugOptionPanelColors(debugStatus);
+    }
+    
+    public void loadStyleFile(String file){
+        nifty.loadStyleFile(file);
+    }
+    
+    public void loadControlFile(String file){
+        nifty.loadControlFile(file); 
+    }
+    
+    public void goToScreen(String screenName){
+        nifty.gotoScreen(screenName);
+    }
+    
+    public void addScreen(String screenName, Screen screen){
+        nifty.addScreen(screenName, screen);
+    }
+    
+    public Nifty getNifty(){
+        return nifty;
     }
 }
