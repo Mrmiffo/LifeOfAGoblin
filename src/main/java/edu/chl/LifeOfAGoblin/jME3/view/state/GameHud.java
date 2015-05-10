5630f2f58583922ea123df7821971a43183bde98
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 18:35:52 +0200

    Created gamehud class, to replace main menu when game starts instead as walkaround for bug

---
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/view/state/GameHud.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.jME3.view.state;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+
+/**
+ *
+ * @author Anton
+ */
+public class GameHud {
+
+}
