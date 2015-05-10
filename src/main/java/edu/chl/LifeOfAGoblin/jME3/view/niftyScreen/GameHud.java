03046006f22dcb60d7714a1d3bfc091c9c6a68c6
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 20:17:48 +0200

    Created game hud

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/view/niftyScreen/GameHud.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/view/niftyScreen/GameHud.java
@@ -2,16 +2,45 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.chl.LifeOfAGoblin.jME3.view.state;
+package edu.chl.LifeOfAGoblin.jME3.view.niftyScreen;
 
-import com.jme3.app.Application;
-import com.jme3.app.state.AbstractAppState;
-import com.jme3.app.state.AppStateManager;
+import de.lessvoid.nifty.builder.ScreenBuilder;
+import de.lessvoid.nifty.screen.Screen;
+import edu.chl.LifeOfAGoblin.jME3.controller.GameHudController;
+import edu.chl.LifeOfAGoblin.jME3.controller.MainMenuController;
+import edu.chl.LifeOfAGoblin.jME3.utils.NiftyGUIWrapper;
+import edu.chl.LifeOfAGoblin.jME3.view.niftyScreen.interfaces.INiftyScreen;
 
 /**
  *
  * @author Anton
  */
-public class GameHud {
+public class GameHud implements INiftyScreen{
+
+    private String gameHudName;
+    private Screen gameHudScreen;
+    
+    public GameHud(){
+        gameHudName = "GameHud";
+        setupGameHud();
+    }
+    
+    @Override
+    public Screen getScreen() {
+        return gameHudScreen;
+    }
+
+    @Override
+    public String getScreenName() {
+        return gameHudName;
+    }
+
+    private void setupGameHud() {
+        final GameHudController gameHudController = new GameHudController(this);
+        gameHudScreen = new ScreenBuilder(gameHudName) {{
+            controller(gameHudController);
+            
+        }}.build(NiftyGUIWrapper.getInstance().getNifty()); //mainMenu
+    }
 
 }
