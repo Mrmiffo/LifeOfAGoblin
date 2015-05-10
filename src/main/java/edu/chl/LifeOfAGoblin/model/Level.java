a7764972eb8e3c853890cb93a6b9a01de0b2649a
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 17:32:49 +0200

    Refactored level. Level will now contain a player model object and the level name

--- src/main/java/edu/chl/LifeOfAGoblin/model/Level.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/Level.java
@@ -5,17 +5,15 @@
 package edu.chl.LifeOfAGoblin.model;
 
 import edu.chl.LifeOfAGoblin.model.interfaces.IModeledNode;
-import edu.chl.LifeOfAGoblin.factory.NodeFactory;
 import edu.chl.LifeOfAGoblin.factory.NodeType;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- *
+ * The level model. Contains the model data of the level and the scene name.
  * @author Anton
  */
 public class Level implements IModeledNode{
     private String scene;
+    private Player player;
     
     /**
      * Creates and stores a scene with a specified name and adds all wanted objects to the scene.
@@ -23,26 +21,25 @@ public class Level implements IModeledNode{
      * @param levelName the name of the level that should be created
      */
     
-    public Level(String levelName){
+    public Level(String levelName, Player player){
         //Save the scene file to load.
         scene = levelName;
+        this.player = player;
 
     }
     
     @Override
-    public String getModel() {
+    public String getModelName() {
         return scene;
     }
 
-    @Override
-    public Map<String, Object> getNodeData() {
-        Map<String, Object> nodeData = new HashMap<>();
-        
-        return nodeData;
-    }
 
     @Override
     public NodeType getNodeType() {
         return NodeType.LEVEL;
+    }
+    
+    public Player getPlayer(){
+        return player;
     }
 }
