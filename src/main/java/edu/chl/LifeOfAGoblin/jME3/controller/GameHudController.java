02955c133689f9967f42f040b52d06e0b98f099c
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 20:13:29 +0200

    Created gameHudController for new game hud

---
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/GameHudController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.jME3.controller;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+import edu.chl.LifeOfAGoblin.jME3.view.niftyScreen.GameHud;
+
+/**
+ *
+ * @author Anton
+ */
+public class GameHudController implements ScreenController{
+
+    public GameHudController(GameHud gameHud) {
+        
+    }
+
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        
+    }
+
+    @Override
+    public void onStartScreen() {
+        
+    }
+
+    @Override
+    public void onEndScreen() {
+        
+    }
+    
+}
