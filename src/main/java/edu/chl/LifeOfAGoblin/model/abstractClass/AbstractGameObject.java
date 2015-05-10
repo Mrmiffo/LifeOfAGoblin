f54e89d4bb8226f08796716e4d47e58f620604a8
Tandl-os <freb@student.chalmers.se>
2015-05-10 20:28:12 +0200

    added abstract class AbstractGameObject

---
+++ src/main/java/edu/chl/LifeOfAGoblin/model/abstractClass/AbstractGameObject.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.model.abstractClass;
+
+/**
+ *
+ * @author fredrik
+ */
+public class AbstractGameObject {
+    public float getModelShapeWidth;
+    public float getModelShapeHeight;
+}
