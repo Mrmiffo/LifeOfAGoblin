6f4e5b181e332b00e3e364bf0ec801510272dbc5
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 20:17:06 +0200

    Made resrouces into assetloader

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/utils/Resources.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/utils/Resources.java
@@ -1,6 +1,9 @@
 package edu.chl.LifeOfAGoblin.jME3.utils;
 
+import com.jme3.asset.AssetManager;
+import com.jme3.asset.plugins.FileLocator;
 import com.jme3.scene.Spatial;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,11 +14,13 @@ import java.util.Map;
 public class Resources {
     private static Resources instance;
     private Map<String, Spatial> geometries = new HashMap<>();
+    private String defaultPath;
+    private AssetManager assetManager;
     
 
     
     private Resources(){
-        
+        defaultPath = "src" + File.separator + "main" + File.separator + "java" + File.separator + "edu" + File.separator + "chl" + File.separator + "LifeOfAGoblin" + File.separator + "assets";
     }
 
     public static synchronized Resources getInstance(){
@@ -25,11 +30,24 @@ public class Resources {
         return instance;
     }
     
-    public void addResource(String name, Spatial resource){
+    public void initialize(AssetManager assetManager){
+        this.assetManager = assetManager;
+    }
+    
+    private void addResource(String name, Spatial resource){
         geometries.put(name, resource.clone());
+    }
+    
+    public void loadResource(String name, String folder){
+        assetManager.registerLocator(defaultPath + File.separator + folder, FileLocator.class);
+        addResource(name, assetManager.loadModel(name));
     }
     
     public Spatial getResources(String name){
         return geometries.get(name).clone();
+    }
+    
+    public void setTempPath(String path){
+        assetManager.registerLocator(defaultPath + File.separator + path, FileLocator.class);
     }
 }
