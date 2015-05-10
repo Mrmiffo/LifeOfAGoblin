b0940d4ff75a6221001640a2392d6eaa7c797dc0
Tandl-os <freb@student.chalmers.se>
2015-05-10 20:25:31 +0200

    added interface ICollidable

---
+++ src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/ICollidable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.model.interfaces;
+
+/**
+ *
+ * @author fredrik
+ */
+public interface ICollidable {
+    public float getHeight();
+    public float getWidth();
+    public void Collision();
+}
