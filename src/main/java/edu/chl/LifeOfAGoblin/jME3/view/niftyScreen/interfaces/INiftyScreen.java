3b5517c8e5892bcdc9ff9252186511a691882e04
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-10 20:14:27 +0200

    Added interface for screens

---
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/view/niftyScreen/interfaces/INiftyScreen.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.jME3.view.niftyScreen.interfaces;
+
+import de.lessvoid.nifty.screen.Screen;
+
+/**
+ * An interface used for setting up nifty screens.
+ * @author Anton
+ */
+public interface INiftyScreen {
+    public Screen getScreen();
+    public String getScreenName();
+}
