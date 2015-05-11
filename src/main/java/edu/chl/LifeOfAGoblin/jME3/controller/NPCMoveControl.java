e1d8147a361a00740d1f5d4717b1ff7f117bc7c5
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-05-11 13:59:27 +0200

    added NPC to NodeFactory

---
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/NPCMoveControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.jME3.controller;
+
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.control.AbstractControl;
+
+/**
+ *
+ * @author kakan
+ */
+public class NPCMoveControl extends AbstractControl{
+
+    @Override
+    protected void controlUpdate(float f) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
