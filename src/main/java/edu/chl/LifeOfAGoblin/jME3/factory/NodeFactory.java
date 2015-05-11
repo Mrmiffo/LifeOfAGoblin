f64a7bc818e8b14d9668383719d6671f27bb5930
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-05-11 17:47:45 +0200

    refactored NodeFactory. Needs more implementation. Might need different interfaces.

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/factory/NodeFactory.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/factory/NodeFactory.java
@@ -15,8 +15,9 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import edu.chl.LifeOfAGoblin.jME3.controller.AbstractMoveControl;
 import edu.chl.LifeOfAGoblin.jME3.controller.ModelControl;
-import edu.chl.LifeOfAGoblin.jME3.controller.NPCMoveControl;
 import edu.chl.LifeOfAGoblin.jME3.controller.PlayerMoveControl;
 import edu.chl.LifeOfAGoblin.model.Level;
 import edu.chl.LifeOfAGoblin.model.Player;
@@ -24,7 +25,6 @@ import edu.chl.LifeOfAGoblin.model.interfaces.IModeledNode;
 import edu.chl.LifeOfAGoblin.jME3.utils.InputManagerWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.PhysicsWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.Resources;
-import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractNPC;
 
 /**
  * A factory used to create a level node and fill existing nodes with data depending on the node type.
@@ -32,7 +32,63 @@ import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractNPC;
  * @author Anton
  */
 public class NodeFactory {
-    public static Node createModeledNode(IModeledNode nodeToCreate){
+    
+    public static Node createNode(NodeType nodetype){
+        switch (nodetype) {
+            case PLAYER:
+            case NPC:
+            case LEVEL:
+                return createModeledNode(nodetype);
+            case CHECKPOINT:
+            case SPAWNPOINT:
+                return createPoint(nodetype);
+            default:
+            throw new InternalError("Error in NodeFactory: createNode()");
+        }
+    }
+    
+    private static Node createModeledNode(NodeType nodetype) {
+        
+        Node node = new Node();
+        IModeledNode nodeToCreate;
+        AbstractMoveControl amc;
+        switch (nodetype) {
+            case PLAYER:
+                nodeToCreate = Player.getInstance();
+                amc = new PlayerMoveControl();
+                InputManagerWrapper.getInstance().registerListener((PlayerMoveControl) amc);
+                break;
+//              add for various NPCs and level
+            default:
+                throw new InternalError("Error in NodeDactory: createModeledNode()");
+        }
+        Spatial model = Resources.getInstance().getResources(nodeToCreate.getModelName());
+        node.attachChild(model);
+        node.addControl(new ModelControl(nodeToCreate));
+        CapsuleCollisionShape shape;
+        CharacterControl mover; //CharacterControl has been depricated prematurly due to BetterCharacterControl. Although BetterCharacterControl contains major flaws (such as missing step height) that make CharacterControl a better choice for this project.
+        
+        //Moving the model node slightly to fit the CollisionShape
+        model.setLocalTranslation(new Vector3f(0, -nodeToCreate.getHeight(), 0));
+        shape = new CapsuleCollisionShape(nodeToCreate.getWidth(), nodeToCreate.getHeight(), 1);
+        mover = new CharacterControl(shape, 0.05f);
+        PhysicsWrapper.getInstance().add(mover);
+        
+        //Set the character jumpspeed. 12 is just right for this character to jump 1f.
+        mover.setJumpSpeed(nodeToCreate.getJumpStrength());
+        
+        //Attaching controls:
+        node.addControl(mover);
+        node.addControl(amc);
+        return node;
+
+    }
+    
+    private static Node createPoint(NodeType nodetype) {
+        return null;
+    }
+        
+        /*
         //TODO Do not create new node in factory, the level will have a node from the scene composer which will be updated instead.
         Node node = new Node();
         node.attachChild(Resources.getInstance().getResources(nodeToCreate.getModelName()));
@@ -80,7 +136,7 @@ public class NodeFactory {
                 break;
         }
         return node;
-    }
+    }*/
     
     public static Node createModeledLevelNode(Level levelToCreate, Camera cam){
         Node node = new Node();
@@ -88,7 +144,7 @@ public class NodeFactory {
         node.addControl(new ModelControl(levelToCreate));
         
         //Adding the player character to the level
-        Node playerNode = NodeFactory.createModeledNode(((Level)levelToCreate).getPlayer());
+        Node playerNode = NodeFactory.createNode(NodeType.PLAYER/*((Level)levelToCreate).getPlayer()*/);
         ChaseCamera chaseCam = new ChaseCamera(cam);
         chaseCam.setRotationSensitivity(0);
         chaseCam.setDefaultHorizontalRotation(new Float(Math.PI/2));
--- src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/IModeledNode.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/IModeledNode.java
@@ -10,4 +10,10 @@ package edu.chl.LifeOfAGoblin.model.interfaces;
  */
 public interface IModeledNode extends INode {
     public String getModelName();
+
+    public float getHeight();
+
+    public float getWidth();
+
+    public float getJumpStrength();
 }
