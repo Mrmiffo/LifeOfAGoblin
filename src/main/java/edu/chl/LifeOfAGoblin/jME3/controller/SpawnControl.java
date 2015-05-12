9929fe538eadd615c4acc88180141ddb9d3d9b88
Tandl-os <freb@student.chalmers.se>
2015-05-13 00:41:58 +0200

    added functionality for spawning

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/SpawnControl.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/SpawnControl.java
@@ -4,20 +4,34 @@
  */
 package edu.chl.LifeOfAGoblin.jME3.controller;
 
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
+import com.jme3.scene.control.AbstractControl;
 import edu.chl.LifeOfAGoblin.jME3.factory.NodeFactory;
 import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
 import edu.chl.LifeOfAGoblin.model.interfaces.ISpawnControl;
 
 /**
- *
+ * A control that belongs to a SpawnPoint. It is responsible for creating new
+ * spawnables and attaching them to the level.
  * @author fredrik
  */
-public class SpawnControl implements ISpawnControl{
+public class SpawnControl extends AbstractControl implements ISpawnControl{
    @Override
    public void Spawn(int amount, NodeType type){
-//    Node node =NodeFactory.createModeledNode(type.);
-//    this.getParent().atttachChild(node);
-//node.setLocalTranslataion(this.getLocalTranslation);
+    Node node = NodeFactory.createNode(type);
+    this.getSpatial().getParent().attachChild(node);
+    node.setLocalTranslation(this.getSpatial().getLocalTranslation());
    }
+
+    @Override
+    protected void controlUpdate(float f) {
+        //probably not needed, active mvc ftw
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+     //probably not needed    
+    }
 }
