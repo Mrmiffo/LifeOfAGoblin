ce48a5888a414c132010398284b9aeead835a845
Tandl-os <freb@student.chalmers.se>
2015-05-13 00:35:14 +0200

    rewrote comments for ICollidable and ISpawnControl

--- src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/ICollidable.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/ICollidable.java
@@ -5,9 +5,14 @@
 package edu.chl.LifeOfAGoblin.model.interfaces;
 
 /**
- *
+ * Interface for Collidable objects, objects implementing this interface 
+ * have methods describing what happens when it collides with another 
+ * ICollidable
  * @author fredrik
  */
 public interface ICollidable extends INode{
+    /**
+     * is Called when another ICollidable collides with this object. 
+     */
     public void collide();
 }
