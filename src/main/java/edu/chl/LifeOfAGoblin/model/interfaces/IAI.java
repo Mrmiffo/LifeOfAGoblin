80284db648e20ff82d565233d2dd17e1d5499a12
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-12 15:49:22 +0200

    Added an interface for handling active AIActions

---
+++ src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/IAI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.model.interfaces;
+
+import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
+import edu.chl.LifeOfAGoblin.model.AIAction;
+
+/**
+ *
+ * @author Ulrika
+ */
+public interface IAI {
+    
+    /**
+     * Returns the currently active type of AIAction.
+     * @return the active AIAction
+     */
+    public AIAction getAIAction();
+    
+    /**
+     * Gets the distance to a specified object and the object's NodeType.
+     * Updates the active AIAction accordingly.
+     */
+    public void updateAIAction(float distance, NodeType type);
+}
