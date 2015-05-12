d20f8cdfe11da8943e13b8a5745d5c895e08a5d1
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-12 15:44:56 +0200

    Added enum for AI actions

---
+++ src/main/java/edu/chl/LifeOfAGoblin/model/AIAction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.model;
+
+/**
+ *
+ * @author Ulrika
+ */
+public enum AIAction {
+    IDLE,
+    MOVETOTARGET,
+    MELEE_ATTACK,
+    RANGED_ATTACK;
+}
