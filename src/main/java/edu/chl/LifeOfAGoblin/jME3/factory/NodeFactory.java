23937edbf9b7bd82f7738d14ebdfce6de35de305
Tandl-os <freb@student.chalmers.se>
2015-05-13 16:21:26 +0200

    implemented functionality in method createModeledLevelNode

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/factory/NodeFactory.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/factory/NodeFactory.java
@@ -5,13 +5,16 @@
 package edu.chl.LifeOfAGoblin.jME3.factory;
 
 import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.input.ChaseCamera;
 import com.jme3.light.AmbientLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import edu.chl.LifeOfAGoblin.jME3.controller.ModelControl;
 import edu.chl.LifeOfAGoblin.model.Level;
 import edu.chl.LifeOfAGoblin.model.Player;
@@ -19,6 +22,7 @@ import edu.chl.LifeOfAGoblin.jME3.utils.PhysicsWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.Resources;
 import edu.chl.LifeOfAGoblin.model.Boss;
 import edu.chl.LifeOfAGoblin.model.Minion;
+import java.util.List;
 
 /**
  * A factory used to create a level node and fill existing nodes with data depending on the node type.
@@ -36,113 +40,63 @@ public class NodeFactory {
             case BOSS:
                 return CharacterFactory.createCharacter(new Boss());
 //            case CHECKPOINT:
-//                return CollisionObjectFactory.createCollisionObject(new Checkpoint());
+//                return CollisionObjectPainter.createCollisionObject(new Checkpoint());
 //            case SPAWNPOINT:
-//                return CollisionObjectFactory.createCollisionObject(new Spawnpoint());
+//                return CollisionObjectPainter.createCollisionObject(new Spawnpoint());
 //            case LEVEL:
 //                return createModeledLevelNode(null, null);
             default:
             throw new InternalError("Error in NodeFactory: createNode()");
         }
     }
-    
-    private static Node createPoint(NodeType nodetype) {
-        return null;
-    }
-        
-        /*
-        //TODO Do not create new node in factory, the level will have a node from the scene composer which will be updated instead.
-        Node node = new Node();
-        node.attachChild(Resources.getInstance().getResources(nodeToCreate.getModelName()));
-        //Attach the model to the node through a custom control containing and providing the model.
-        node.addControl(new ModelControl(nodeToCreate));
-        
-        CapsuleCollisionShape shape;
-        CharacterControl mover; //CharacterControl has been depricated prematurly due to BetterCharacterControl. Although BetterCharacterControl contains major flaws (such as missing step height) that make CharacterControl a better choice for this project.
-        
-        switch (nodeToCreate.getNodeType()){
-            case PLAYER:
-                //Moving the model node slightly to fit the CollisionShape
-                node.getChild(0).setLocalTranslation(new Vector3f(0,-((Player)nodeToCreate).getHeight(),0));
-                //Setting upp collision shape and character control:
-                shape = new CapsuleCollisionShape(((Player)nodeToCreate).getWidth(), ((Player)nodeToCreate).getHeight(), 1);
-                mover = new CharacterControl(shape, 0.05f);
-                PhysicsWrapper.getInstance().add(mover);
-                //Set the character jumpspeed. 12 is just right for this character to jump 1f.
-                mover.setJumpSpeed(12);
-                //Setting up PlayerMoveControl which translates keycommands(key binds) to node actions.
-                PlayerMoveControl pmc = new PlayerMoveControl();
-                InputManagerWrapper.getInstance().registerListener(pmc);
-                //Attaching controls:
-                node.addControl(mover);
-                node.addControl(pmc);
-                break;
-            case NPC:
-                //Moving the model node slightly to fit the CollisionShape
-                node.getChild(0).setLocalTranslation(new Vector3f(0,-((AbstractNPC)nodeToCreate).getHeight(),0));
-                //Setting upp collision shape and character control:
-                shape = new CapsuleCollisionShape(((AbstractNPC)nodeToCreate).getWidth(), ((AbstractNPC)nodeToCreate).getHeight(), 1);
-                mover = new CharacterControl(shape, 0.05f);
-                PhysicsWrapper.getInstance().add(mover);
-                //Set the character jumpspeed. 12 is just right for this character to jump 1f.
-                mover.setJumpSpeed(12);
-                //Setting up PlayerMoveControl which translates AI behaviour to node actions.
-                NPCMoveControl nmc = new NPCMoveControl();
-                //Attaching controls:
-                node.addControl(mover);
-                node.addControl(nmc);      
-                break;
-            case CHECKPOINT:
-                break;
-            case SPAWNPOINT:
-                break;
-        }
-        return node;
-    }*/
-    
+ 
+    /**
+     * creates a 
+     * @param levelToCreate
+     * @param cam
+     * @return 
+     */
     public static Node createModeledLevelNode(Level levelToCreate, Camera cam){
-        Node node = new Node();
-        node.attachChild(Resources.getInstance().getResources(levelToCreate.getModelName()));
-        node.addControl(new ModelControl(levelToCreate));
-        
-        //Adding the player character to the level
-        Node playerNode = NodeFactory.createNode(NodeType.PLAYER/*((Level)levelToCreate).getPlayer()*/);
-        ChaseCamera chaseCam = new ChaseCamera(cam);
-        chaseCam.setRotationSensitivity(0);
-        chaseCam.setDefaultHorizontalRotation(new Float(Math.PI/2));
-        playerNode.addControl(chaseCam); //Adding a camera control to make the camera follow the player
-        node.attachChild(playerNode);
-        node.setLocalTranslation(0f, -5f, 0f);
-        
+        Node levelNode = ((Node)Resources.getInstance().getResources(levelToCreate.getModelName()));
+        List<Spatial> nodeList = levelNode.getChildren();
+        for(int i = 0; i<nodeList.size(); i++){
+            System.out.println("node är " + nodeList.get(i).toString());
+            System.out.println(nodeList.get(i).getUserDataKeys().toString());
+            System.out.println("noden i childrens localtranslation är " + nodeList.get(i).getLocalTranslation());
+            if(((Node)nodeList.get(i)).getUserDataKeys().size() > 0){
+                String type = ((String)((Node)nodeList.get(i)).getUserData("nodeType"));
+                switch(type){
+                    case("SPAWNPOINT"):
+                        CollisionObjectPainter.paintCollisionObject(NodeType.SPAWNPOINT, ((Node)nodeList.get(i)));
+                        break;
+                    case("CHECKPOINT"):
+                        CollisionObjectPainter.paintCollisionObject(NodeType.CHECKPOINT, ((Node)nodeList.get(i)));
+                        break;
+                    case("GAMEOBJECT"):
+                        CollisionObjectPainter.paintCollisionObject(NodeType.GAMEOBJECT, ((Node)nodeList.get(i)));
+                    case("PLAYER"):  
+                        //Adding the player character to the level
+                        Node playerNode = NodeFactory.createNode(NodeType.PLAYER/*((Level)levelToCreate).getPlayer()*/);
+                        ChaseCamera chaseCam = new ChaseCamera(cam);
+                        chaseCam.setRotationSensitivity(0);
+                        chaseCam.setDefaultHorizontalRotation(new Float(Math.PI/2));
+                        playerNode.addControl(chaseCam); //Adding a camera control to make the camera follow the player
+                        ((Node)nodeList.get(i)).attachChild(playerNode);
+                        
+                        System.out.println("noden i childrens localtranslation är " + nodeList.get(i).getLocalTranslation());
+                        playerNode.getControl(CharacterControl.class).warp(nodeList.get(i).getWorldTranslation());
+                        System.out.println(playerNode.getLocalTranslation());
+                        levelNode.setLocalTranslation(0f, -5f, 0f);
+                        break;
+                }
+            }   
         //Adding collisionshape
-        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(node);
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(levelNode);
         RigidBodyControl landscape = new RigidBodyControl(sceneShape, 0);
         PhysicsWrapper.getInstance().add(landscape);
-        node.addControl(landscape);
+        levelNode.addControl(landscape);        
+    }
+        return levelNode;
 
-        //TODO REMOVE -> FOR TESTING PURPOSES ONLY
-        //Adding a basic ambient light to the level
-//        AmbientLight l = new AmbientLight();
-//        l.setColor(ColorRGBA.White.mult(3f));
-//        node.addLight(l);
-        
-        return node;
-    }
-    /**
-     * Creates and returns a new Node based on a type and userData from another
-     * node.
-     * @param type The type of node to be created.
-     * @param node The node containing the userData.
-     * @return A new node of a specified NodeType with a model that matches the
-     * parameter node's userData
-     */
-    public static Node createUserDataNode(NodeType type, Node node){
-        switch (type) {
-            case CHECKPOINT:
-                return CollisionObjectFactory.createCollisionObject(type, node);
-            case SPAWNPOINT:
-               return CollisionObjectFactory.createCollisionObject(type, node);
-        }
-        return null;
-    }
+}
 }
