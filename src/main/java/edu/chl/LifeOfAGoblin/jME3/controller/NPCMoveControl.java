61b29002cad78463a442b663d36f349f08f5dbc1
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-13 08:14:52 +0200

    Added logic for deciding direction when moving to target

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/NPCMoveControl.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/NPCMoveControl.java
@@ -4,14 +4,54 @@
  */
 package edu.chl.LifeOfAGoblin.jME3.controller;
 
+import com.jme3.scene.Node;
+import edu.chl.LifeOfAGoblin.model.AIAction;
+import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractNPC;
+import edu.chl.LifeOfAGoblin.model.interfaces.INode;
+
 /**
  *
  * @author Ulrika Uddeborg
  */
 public class NPCMoveControl extends AbstractMoveControl {
-
+ 
+    private AbstractNPC npcModel;
     private float idleMoveRange;
     
+    public NPCMoveControl() {
+        INode n = this.spatial.getControl(ModelControl.class).getModel();
+        if (n.getClass() == AbstractNPC.class) {
+            npcModel = (AbstractNPC)n;
+        }
+    }
     
+    public NPCMoveControl(float idleMoveRange) {
+        this();
+        this.idleMoveRange = 1; //Start value
+    }
+    
+    @Override
+    public void controlUpdate(float tpf) {
+        AIAction action = npcModel.getAIAction();
+        switch (action) {
+            case IDLE:
+                if (idleMoveRange == 0 && left != right) {
+                    right = !right;
+                    left = !left;
+                    idleMoveRange = 2;
+                }
+                break;
+            case MOVETOTARGET:
+                String direction = npcModel.getTargetDirection();
+                if (direction.equals("left")) {
+                    left = true;
+                    right = false;
+                } else if (direction.equals("right")) {
+                    right = true;
+                    left = false;
+                }
+        }
+        super.controlUpdate(tpf);
+    }
     
 }
---
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/NPCRangeControl.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.jME3.controller;
+
+import com.jme3.bullet.collision.PhysicsCollisionEvent;
+import com.jme3.bullet.collision.PhysicsCollisionListener;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.control.GhostControl;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
+import edu.chl.LifeOfAGoblin.jME3.utils.PhysicsWrapper;
+import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractNPC;
+import edu.chl.LifeOfAGoblin.model.interfaces.INode;
+
+/**
+ *
+ * @author Ulrika
+ */
+public class NPCRangeControl extends GhostControl implements PhysicsCollisionListener {
+
+    public NPCRangeControl() {
+        super(new SphereCollisionShape(3));
+        //TODO Replace with fancy wrapper call
+        PhysicsWrapper.getInstance().getBulletAppState().getPhysicsSpace().addCollisionListener(this);
+        
+        
+    }
+
+    @Override
+    public void collision(PhysicsCollisionEvent pce) {
+        
+        if (pce.getNodeA() == this.spatial) {
+            updateCollisionInfo(pce.getNodeB());
+        } else if (pce.getNodeB() == this.spatial) {
+            updateCollisionInfo(pce.getNodeA());
+        }
+    }
+    
+    /**
+     * 
+     * @param collided the object with which the GhostControl has collided with
+     */
+    private void updateCollisionInfo(Spatial collided) {
+        INode n = this.spatial.getControl(ModelControl.class).getModel();
+        AbstractNPC npcModel = (AbstractNPC)n;
+        
+        Vector3f npcLocation = this.spatial.getLocalTranslation();
+        
+        float distance = npcLocation.distance(collided.getLocalTranslation());
+        
+        float deltaX = npcLocation.getX() - collided.getLocalTranslation().getX();
+        String direction; //TODO Replace with enum?
+        if (deltaX >= 0) {
+            direction = "left";
+        } else {
+            direction = "right";
+        }
+        
+        INode collideModel = collided.getControl(ModelControl.class).getModel();
+
+        npcModel.updateAIAction(distance, direction, collideModel.getNodeType());
+    }
+}
