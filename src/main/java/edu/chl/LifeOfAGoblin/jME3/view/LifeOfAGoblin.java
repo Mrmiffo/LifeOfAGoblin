84984bd3dc7569000f1d9f46d868f1bc224a31ee
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-13 14:18:22 +0200

    Cleaned up the LifeOfAGoblin class

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/view/LifeOfAGoblin.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/view/LifeOfAGoblin.java
@@ -5,7 +5,6 @@
 package edu.chl.LifeOfAGoblin.jME3.view;
 
 import com.jme3.app.SimpleApplication;
-import edu.chl.LifeOfAGoblin.jME3.view.state.GameAppState;
 import edu.chl.LifeOfAGoblin.jME3.utils.InputManagerWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.NiftyGUIWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.Resources;
@@ -21,7 +20,6 @@ public class LifeOfAGoblin extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        //Camera
         NiftyGUIWrapper.getInstance().initialize(this);
         InputManagerWrapper.getInstance().initialize(inputManager);
         StateManagerWrapper.getInstance().initialize(stateManager);
@@ -34,7 +32,6 @@ public class LifeOfAGoblin extends SimpleApplication {
         
         //Disables the built-in flyCam
         this.flyCam.setEnabled(false);
-        //this.getFlyByCamera().setDragToRotate(true);
     }
     
 }
