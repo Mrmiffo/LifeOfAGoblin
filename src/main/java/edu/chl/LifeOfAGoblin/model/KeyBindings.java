1eb3471c19c386c7a3e9dc7363cda2982b479526
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-05-15 09:08:21 +0200

    added static methods to KeyBindings

--- src/main/java/edu/chl/LifeOfAGoblin/model/KeyBindings.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/KeyBindings.java
@@ -10,6 +10,8 @@ import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.input.controls.Trigger;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -20,7 +22,7 @@ public class KeyBindings implements Serializable {
         WALK_LEFT ("walkLeft", new Trigger[] {new KeyTrigger(KeyInput.KEY_A)}),
         WALK_RIGHT ("walkRight", new Trigger[] { new KeyTrigger(KeyInput.KEY_D) }),
         JUMP ("jump", new Trigger[] {
-            new MouseButtonTrigger(MouseInput.BUTTON_LEFT),
+            //new MouseButtonTrigger(MouseInput.BUTTON_LEFT),
             new KeyTrigger(KeyInput.KEY_W),
             new KeyTrigger(KeyInput.KEY_SPACE)
         });
@@ -44,6 +46,19 @@ public class KeyBindings implements Serializable {
         public void setTriggers(Trigger[] triggers) {
             this.triggers = triggers;
         }
-
+    }
+    
+    public static void setDefaultKeyBindings() {
+        KeyBind.WALK_LEFT.setTriggers(new Trigger[] {new KeyTrigger(KeyInput.KEY_A)});
+        KeyBind.WALK_RIGHT.setTriggers(new Trigger[] { new KeyTrigger(KeyInput.KEY_D)});
+        KeyBind.JUMP.setTriggers(new Trigger[] {new KeyTrigger(KeyInput.KEY_W), new KeyTrigger(KeyInput.KEY_SPACE)});  
+    }
+    
+    public static Map<KeyBind, Trigger[]> getCurrentKeyBindings() {
+        Map<KeyBind, Trigger[]> temp = new HashMap<>();
+        for (KeyBind action: KeyBind.values()) {
+            temp.put(action, action.getTriggers());
+        }
+        return temp;
     }
 }
