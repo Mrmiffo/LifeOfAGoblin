b7f7c8c1dc8ede20aff5c9326b650527aa48b509
Tandl-os <freb@student.chalmers.se>
2015-05-17 15:44:19 +0200

    added test for class checkpoint

---
+++ src/test/java/edu/chl/LifeOfAGoblin/model/CheckpointTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.model;
+
+import com.jme3.scene.Node;
+import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fredrik
+ */
+public class CheckpointTest {
+    
+    public CheckpointTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetNodeType() {
+        Checkpoint cp = new Checkpoint(1, 1, 1f);
+        assertTrue(cp.getNodeType().equals(NodeType.CHECKPOINT));
+    }
+
+    @Test
+    public void testCollide() {
+        Checkpoint cp = new Checkpoint(1, 1, 1f);
+        cp.collide();
+        assertTrue(cp.getIsActivated());
+        cp.collide();
+        assertTrue(cp.getIsActivated());
+    }
+
+    @Test
+    public void testUpdateProgress() {
+    }
+}
