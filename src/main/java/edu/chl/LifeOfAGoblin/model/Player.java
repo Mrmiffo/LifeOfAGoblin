866c16eb3f62ed2fdfd0ab656f00e371c33d10ef
Tandl-os <freb@student.chalmers.se>
2015-05-18 22:01:54 +0200

    added functionality in collide and boolean isDamaged

--- src/main/java/edu/chl/LifeOfAGoblin/model/Player.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/Player.java
@@ -7,6 +7,7 @@ package edu.chl.LifeOfAGoblin.model;
 import edu.chl.LifeOfAGoblin.jME3.controller.GameHudController;
 import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractCharacter;
 import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
+import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractNPC;
 
 /**
  * The player calss represent the player character in the game. The class 
@@ -34,8 +35,15 @@ public class Player extends AbstractCharacter {
         return NodeType.PLAYER;
     } 
     
+    
     @Override
     public void collide(){
+        //todo add methods for colliding with a player
+    }
+    
+    public void collide(AbstractNPC enemy){
+        System.out.println("player");
+        super.setHealth(super.getHealth()-1);
         //todo add methods for colliding with a player
     }
     
