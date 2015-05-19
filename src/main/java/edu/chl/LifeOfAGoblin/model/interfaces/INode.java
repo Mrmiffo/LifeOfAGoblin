cb179e584986a73e64e0182e9884caffcfce5134
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-05-19 10:20:19 +0200

    added javadoc to class AbstractCharacter and interfaces IModeledNode and INode

--- src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/INode.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/INode.java
@@ -7,25 +7,26 @@ package edu.chl.LifeOfAGoblin.model.interfaces;
 import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
 
 /**
- *
+ * An interface descibing an object that can be referenced as a node.
  * @author Anton
  */
 public interface INode {
+    
     /**
-     * Is this needed? Node types are added directly to the Node in the current design(150515)...
-     * @return 
+     * Returns what type of node the implementor is.
+     * @return the nodetype of the implementor.
      */
-    public NodeType getNodeType();
+    public NodeType getNodeType(); //Is this needed? Node types are added directly to the Node in the current design(150515)...
+    
     /**
-     * Returns the height of the model.
-     * Should this method be part of the IModeledNode interface?
-     * @return 
+     * Returns the height of the implementor's model.
+     * @return the height of the model
      */
-    public float getHeight();
+    public float getHeight(); //Should this method be part of the IModeledNode interface?
+    
     /**
-     * Returns the width of the model.
-     * Should this method be part of the IModeldNode interface?
-     * @return 
+     * Returns the width of the implementor's model.
+     * @return the width of the model.
      */
-    public float getWidth();
+    public float getWidth(); //Should this method be part of the IModeldNode interface?
 }
