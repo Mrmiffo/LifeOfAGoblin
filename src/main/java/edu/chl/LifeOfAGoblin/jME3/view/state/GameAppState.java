76f56447301b340bdacf6679b30069ba41407c7c
Tandl-os <freb@student.chalmers.se>
2015-05-19 18:43:13 +0200

    added functionality for listening to pauseinput from player

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/view/state/GameAppState.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/view/state/GameAppState.java
@@ -6,15 +6,20 @@ import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Node;
+import edu.chl.LifeOfAGoblin.jME3.controller.interfaces.IKeyListener;
 import edu.chl.LifeOfAGoblin.model.Level;
 import edu.chl.LifeOfAGoblin.jME3.factory.NodeFactory;
+import edu.chl.LifeOfAGoblin.jME3.utils.InputManagerWrapper;
+import edu.chl.LifeOfAGoblin.jME3.utils.NiftyGUIWrapper;
+import edu.chl.LifeOfAGoblin.jME3.utils.StateManagerWrapper;
+import edu.chl.LifeOfAGoblin.model.Actions;
 import edu.chl.LifeOfAGoblin.utils.LevelManager;
 
 /**
  * The game appstate is the state in which the game itself is run and initialized. 
  * @author Anton
  */
-public class GameAppState extends AbstractAppState {
+public class GameAppState extends AbstractAppState implements IKeyListener {
     private Node rootNode;
     private Application app;
     private int levelToStart;
@@ -38,6 +43,7 @@ public class GameAppState extends AbstractAppState {
         super.initialize(stateManager, app);
         rootNode = ((SimpleApplication)app).getRootNode();
         this.app = app;
+        InputManagerWrapper.getInstance().registerListener(this);
         
         //Starts the level and sets the background
         startLevel();
@@ -75,5 +81,27 @@ public class GameAppState extends AbstractAppState {
         Level level = LevelManager.getInstance().getLevel(levelToStart);
         Node levelNode = NodeFactory.createModeledLevelNode(level, app.getCamera());
         rootNode.attachChild(levelNode);
+    }
+
+    private final Actions[] actions = new Actions[] {
+        Actions.OPEN_MENU
+    };
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals(Actions.OPEN_MENU.toString())){
+                pause();       
+        }
+    }
+    
+    public void pause(){
+        System.out.println("pause");
+        NiftyGUIWrapper.getInstance().showPauseMenu();
+        StateManagerWrapper.getInstance().detachCurrentState();
+    }
+
+    @Override
+    public Actions[] getKeyBinds() {
+        return (Actions[])actions.clone();
     }
 }
