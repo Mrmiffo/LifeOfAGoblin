48a35fdedf3811fca1b880ff4e2a0aed7649d308
Marvin043 <marvin.ztrubas@gmail.com>
2015-05-19 10:31:41 +0200

    Setup save load functionality for profile

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/view/LifeOfAGoblin.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/view/LifeOfAGoblin.java
@@ -8,10 +8,14 @@ import com.jme3.app.SimpleApplication;
 import edu.chl.LifeOfAGoblin.jME3.utils.InputManagerWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.NiftyGUIWrapper;
 import edu.chl.LifeOfAGoblin.jME3.utils.Resources;
+import edu.chl.LifeOfAGoblin.utils.SaveLoadManager;
 import edu.chl.LifeOfAGoblin.jME3.utils.StateManagerWrapper;
 import edu.chl.LifeOfAGoblin.jME3.view.niftyScreen.MainMenu;
+import edu.chl.LifeOfAGoblin.jME3.view.niftyScreen.ProfileMenu;
 import edu.chl.LifeOfAGoblin.jME3.view.niftyScreen.SettingsMenu;
+import edu.chl.LifeOfAGoblin.model.Profile;
 import edu.chl.LifeOfAGoblin.utils.LevelManager;
+import java.util.List;
 
 
 /**
@@ -30,15 +34,37 @@ public class LifeOfAGoblin extends SimpleApplication {
         StateManagerWrapper.getInstance().initialize(stateManager);
         Resources.getInstance().initialize(assetManager);
         LevelManager.getInstance().initialize();
+        setupProfile();
         
         //Create an instance of the main menu, add it to nifty and display it.
         MainMenu mainMenu = new MainMenu();
         SettingsMenu settingsMenu = new SettingsMenu();
+        ProfileMenu profileMenu = new ProfileMenu();
         NiftyGUIWrapper.getInstance().addScreen(mainMenu.getScreenName(), mainMenu.getScreen());
         NiftyGUIWrapper.getInstance().goToScreen(mainMenu.getScreenName());
         
         //Disables the built-in flyCam
         this.flyCam.setEnabled(false);
+    }
+
+    /**
+     * Metod to load saved profiles into the static profile list.
+     */
+    private void setupProfile() {
+        List<String> allSavedFiles = SaveLoadManager.getInstance().getSavedFiles(null);
+        if (allSavedFiles.size() == 0){
+            Profile defaultProfile = new Profile("defaultProfile");
+            Profile.addProfile(defaultProfile);
+            Profile.setActiveProfile(defaultProfile);
+        } else {
+            for (String fileName: allSavedFiles){
+                Profile tempProfile = (Profile) SaveLoadManager.getInstance().loadFile(null, fileName);
+                Profile.addProfile(tempProfile);
+                if (tempProfile.getIsActiveProfile()){
+                    Profile.setActiveProfile(tempProfile);
+                }
+            }
+        }
     }
     
 }
