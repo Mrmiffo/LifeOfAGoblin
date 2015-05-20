40486bfd6d898296d40cece9b01eafac78900b92
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-20 13:52:26 +0200

    Changed boolean use to Direction use

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/PlayerMoveControl.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/PlayerMoveControl.java
@@ -2,6 +2,8 @@ package edu.chl.LifeOfAGoblin.jME3.controller;
 
 import edu.chl.LifeOfAGoblin.jME3.controller.interfaces.IKeyListener;
 import edu.chl.LifeOfAGoblin.model.Actions;
+import edu.chl.LifeOfAGoblin.model.Direction;
+import edu.chl.LifeOfAGoblin.model.Player;
 
 /**
  *
@@ -17,15 +19,19 @@ public class PlayerMoveControl extends AbstractMoveControl implements IKeyListen
     public void onAction(String name, boolean isPressed, float tpf) {
         if (name.equals(Actions.WALK_RIGHT.toString())){
             if (isPressed) {
-                right = true;
+                //right = true;
+                currentDirection = Direction.RIGHT;
             } else {
-                right = false;
+                //right = false;
+                currentDirection = Direction.STAND_STILL;
             }
         } else if (name.equals(Actions.WALK_LEFT.toString())){
             if (isPressed) {
-                left = true;
+                //left = true;
+                currentDirection = Direction.LEFT;
             } else {
-                left = false;
+                //left = false;
+                currentDirection = Direction.STAND_STILL;
             }
         } else if (name.equals(Actions.JUMP.toString())){
             jump();
