12f6cc8fcf92ab3201a8c946e4952d13c29a9e66
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-21 11:37:01 +0200

    Added activatable interface

---
+++ src/main/java/edu/chl/LifeOfAGoblin/model/interfaces/IActivatable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.model.interfaces;
+
+/**
+ *
+ * @author Ulrika
+ */
+public interface IActivatable {
+    
+    public void activate();
+    
+    public void inactivate();
+    
+    public boolean isActivated();
+
+}
