3200b44ef620504bc1282397c66e29b276db736b
Tandl-os <freb@student.chalmers.se>
2015-05-21 14:22:30 +0200

    made the test work

--- src/test/java/edu/chl/LifeOfAGoblin/jME3/controller/SpawnControlTest.java
+++ src/test/java/edu/chl/LifeOfAGoblin/jME3/controller/SpawnControlTest.java
@@ -4,20 +4,19 @@
  */
 package edu.chl.LifeOfAGoblin.jME3.controller;
 
-import edu.chl.LifeOfAGoblin.jME3.factory.NodeFactory;
+import com.jme3.app.state.AppStateManager;
 import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 import edu.chl.LifeOfAGoblin.jME3.utils.Resources;
 import edu.chl.LifeOfAGoblin.jME3.view.LifeOfAGoblin;
 import com.jme3.asset.AssetManager;
-import com.jme3.asset.plugins.FileLocator;
 import com.jme3.scene.Node;
+import com.jme3.system.JmeSystem;
+import edu.chl.LifeOfAGoblin.jME3.utils.StateManagerWrapper;
 
 /**
  *
@@ -26,6 +25,8 @@ import com.jme3.scene.Node;
 public class SpawnControlTest {
     private Node lvl;
     private LifeOfAGoblin loag;
+    AssetManager assetManager = null;
+    AppStateManager stateManager = null;
     
     public SpawnControlTest() {
     }
@@ -42,19 +43,24 @@ public class SpawnControlTest {
     public void setUp() {
         this.lvl = new Node();
         this.loag = new LifeOfAGoblin();
-       // loag.simpleInitApp();
-        Resources.getInstance().initialize(loag.getAssetManager());
-        System.out.println(loag.getAssetManager()==null);
+        this.assetManager = JmeSystem.newAssetManager(Thread.currentThread().getContextClassLoader().getResource("com/jme3/asset/Desktop.cfg"));
+        this.stateManager = new AppStateManager(loag);
+        StateManagerWrapper.getInstance().initialize(stateManager);
+        Resources.getInstance().initialize(assetManager);
     }
     
     @After
     public void tearDown() {
     }
 
+
     @Test
+    
     public void testSpawn() {
         SpawnControl control = new SpawnControl();
-        lvl.addControl(control);
+        Node spawnPoint = new Node();
+        lvl.attachChild(spawnPoint);
+        spawnPoint.addControl(control);
         control.Spawn(1, NodeType.MINION);
     }
 
