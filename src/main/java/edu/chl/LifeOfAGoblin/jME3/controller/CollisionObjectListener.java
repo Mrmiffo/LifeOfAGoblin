05675b66864424d0fcc21198f3a37547b37dde0e
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-21 10:39:07 +0200

    Cleaned up the class

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/CollisionObjectListener.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/CollisionObjectListener.java
@@ -10,14 +10,14 @@ import com.jme3.bullet.collision.PhysicsCollisionListener;
 import com.jme3.bullet.collision.PhysicsCollisionObject;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
 import com.jme3.scene.control.AbstractControl;
 import edu.chl.LifeOfAGoblin.model.Player;
 import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractCollisionObject;
 import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractNPC;
+import edu.chl.LifeOfAGoblin.model.interfaces.INode;
 
 /**
- *
- * @author Fredrik
  * A class that represents a control that listens to collisions with a player 
  * and an ICollidable and runs collide in the ICollidable
  * if all requirements are fulfilled.
@@ -38,18 +38,15 @@ public class CollisionObjectListener extends AbstractControl implements PhysicsC
  */
     @Override
     public void collision(PhysicsCollisionEvent pce) {
-        if(pce.getNodeB().getUserDataKeys().contains("nodeType")){
-            if(collide(pce.getObjectA(), pce.getObjectB())){
-                if(pce.getNodeB().getControl(ModelControl.class).getModel() instanceof AbstractCollisionObject)
-                if(!((AbstractCollisionObject)pce.getNodeB().getControl(ModelControl.class).getModel()).getIsActivated()){
-                        ((AbstractCollisionObject)pce.getNodeB().getControl(ModelControl.class).getModel()).collide();
-                    }
-                
-                if(pce.getNodeB().getControl(ModelControl.class).getModel() instanceof AbstractNPC){
-                    if(!((Player)pce.getNodeA().getControl(ModelControl.class).getModel()).getIsDamaged()){
-                        ((Player)pce.getNodeA().getControl(ModelControl.class).getModel()).collide((AbstractNPC) pce.getNodeB().getControl(ModelControl.class).getModel());
-                    }   
-                }
+        if (pce.getNodeA().getUserDataKeys().contains("nodeType")) {
+            if (collide(pce.getObjectA(), pce.getObjectB())) {
+                doCollision((Node)pce.getNodeA(), (Node)pce.getNodeB());
+            }
+        }
+        
+        if (pce.getNodeB().getUserDataKeys().contains("nodeType")) {
+            if (collide(pce.getObjectB(), pce.getObjectA())) {
+                doCollision((Node)pce.getNodeB(), (Node)pce.getNodeA());
             }
         }
     }
@@ -61,12 +58,33 @@ public class CollisionObjectListener extends AbstractControl implements PhysicsC
  */
     @Override
     public boolean collide(PhysicsCollisionObject pco, PhysicsCollisionObject pco1) {
-        if(pco.getCollideWithGroups() > 1 || pco1.getCollisionGroup() > 1){        
-            if(pco.getCollideWithGroups() == (pco1.getCollisionGroup()) || pco1.getCollideWithGroups() == (pco.getCollisionGroup())){
+        if (pco.getCollideWithGroups() > 1 || pco1.getCollisionGroup() > 1) {        
+            if (pco.getCollideWithGroups() == (pco1.getCollisionGroup()) || pco1.getCollideWithGroups() == (pco.getCollisionGroup())){
                 return true;
             }
         }
         return false;
+    }
+    
+    public void doCollision(Node current, Node collided) {
+        INode model = current.getControl(ModelControl.class).getModel();
+        
+        if (model instanceof AbstractCollisionObject) {
+            AbstractCollisionObject ACOmodel = (AbstractCollisionObject)model;
+            if (!ACOmodel.isActivated()) {
+                ACOmodel.collide();
+            }
+        }
+                
+         if (model instanceof AbstractNPC) {
+             INode collidedModel = collided.getControl(ModelControl.class).getModel();
+             if (collidedModel.getClass() == Player.class) {
+                 Player pCollidedModel = (Player)collidedModel;
+                 if (!pCollidedModel.getIsDamaged()) {
+                     pCollidedModel.collide((AbstractNPC)model);
+                 }
+             }
+         }
     }
 
     @Override
