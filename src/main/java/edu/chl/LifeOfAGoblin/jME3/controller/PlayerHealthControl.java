c02066b3cd1651a3b5491a8578f9b1810c683844
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-21 09:10:33 +0200

    Improved formatting

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/PlayerHealthControl.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/PlayerHealthControl.java
@@ -15,21 +15,23 @@ import edu.chl.LifeOfAGoblin.model.Player;
  *
  * @author Anton
  */
-public class PlayerHealthControl extends AbstractControl{
+public class PlayerHealthControl extends AbstractControl {
+    
     private int lastHealth = 0;
     private int lastMaxHealth = 0;
+    
     @Override
     protected void controlUpdate(float tpf) {
         Player player = (Player) spatial.getControl(ModelControl.class).getModel();
-        if (player.getHealth() != lastHealth || player.getMaxHealth() != lastMaxHealth){
+        if (player.getHealth() != lastHealth || player.getMaxHealth() != lastMaxHealth) {
             GameHudController.updateHudHealthbar(player.getHealth(), player.getMaxHealth());
             lastHealth = player.getHealth();
             lastMaxHealth = player.getMaxHealth();
         }
-        if(player.getIsDead()){
-            for(int i = 0; i <spatial.getParent().getParent().getChildren().size(); i++){
-                if(spatial.getParent().getParent().getChildren().get(i).getUserDataKeys().contains("nodeType")){
-                    if(spatial.getParent().getParent().getChildren().get(i).getUserData("nodeType").equals("CHECKPOINT")){
+        if (player.getIsDead()) {
+            for (int i = 0; i < spatial.getParent().getParent().getChildren().size(); i++) {
+                if (spatial.getParent().getParent().getChildren().get(i).getUserDataKeys().contains("nodeType")) {
+                    if (spatial.getParent().getParent().getChildren().get(i).getUserData("nodeType").equals("CHECKPOINT")) {
                   //  && spatial.getParent().getParent().getChildren().get(i).getUserData("NUMBER").equals(Profile.getActiveProfile().getProgress().getLastVisitedCheckpoint())){
                         spatial.getControl(CharacterControl.class).warp(spatial.getParent().getParent().getChildren().get(i).getLocalTranslation());
                         player.setHealth(player.getMaxHealth());
