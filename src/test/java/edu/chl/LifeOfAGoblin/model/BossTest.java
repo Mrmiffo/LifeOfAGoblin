74068f51e706540b0ecca04d0366e7c54bddd8a3
Tandl-os <freb@student.chalmers.se>
2015-05-23 19:25:52 +0200

    added tests for all relevant methods in class

---
+++ src/test/java/edu/chl/LifeOfAGoblin/model/BossTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.LifeOfAGoblin.model;
+
+import edu.chl.LifeOfAGoblin.jME3.factory.NodeType;
+import edu.chl.LifeOfAGoblin.model.abstractClass.AbstractGameObject;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fredrik
+ */
+public class BossTest {
+    
+    public BossTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetNodeType() {
+        // setup
+        Boss boss1 = new Boss();
+        AbstractGameObject boss2 = new Boss();
+        
+        //tests that a boss's nodetype is boss
+        assertTrue(boss1.getNodeType().equals(NodeType.BOSS));
+        assertTrue(boss2.getNodeType().equals(NodeType.BOSS));
+    }
+
+}
