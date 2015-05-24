9b7f7e8bc0a136bb67f4175c3ec7b90ff8a3699a
Tandl-os <freb@student.chalmers.se>
2015-05-24 22:25:24 +0200

    changed to use passive mvc

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/SpawnControl.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/SpawnControl.java
@@ -11,7 +11,10 @@ import com.jme3.scene.Node;
 import com.jme3.scene.control.AbstractControl;
 import edu.chl.LifeOfAGoblin.jME3.factory.NodeFactory;
 import edu.chl.LifeOfAGoblin.model.NodeType;
-import edu.chl.LifeOfAGoblin.model.interfaces.ISpawnControl;
+import edu.chl.LifeOfAGoblin.model.SpawnPoint;
+import edu.chl.LifeOfAGoblin.model.interfaces.IActivatable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A control that belongs to a SpawnPoint. It is responsible for creating new
@@ -20,24 +23,40 @@ import edu.chl.LifeOfAGoblin.model.interfaces.ISpawnControl;
  * @author fredrik
  */
 
-public class SpawnControl extends AbstractControl implements ISpawnControl {
-   
-    @Override
-    public void spawn(int amount, NodeType type) {
+public class SpawnControl extends AbstractControl{
+   private IActivatable activatable;
+   private boolean hasSpawned = false;
+    /**
+     * creates a number of spawnables and puts them in the game
+     * @param amount the number of spawnables to put in the game
+     * @param type the type of spawnable to put in the game
+     */
+    public synchronized void spawn(int amount, NodeType type) {
         if (amount > 0 && type.getSpawnable()) {
-            for (int i = 0; i<amount; i++) {
+        for (int i = 0; i<amount; i++) {
                 Node node = NodeFactory.createNode(type);
-            node.setUserData("nodeType", type.toString());
-            this.getSpatial().getParent().attachChild(node);
-            node.setLocalTranslation(this.getSpatial().getLocalTranslation());
-            node.getControl(CharacterControl.class).warp(this.getSpatial().getLocalTranslation());
+                node.setUserData("nodeType", type.toString());
+                this.getSpatial().getParent().attachChild(node);
+             //   node.setLocalTranslation(this.getSpatial().getLocalTranslation());
+                node.getControl(CharacterControl.class).warp(this.getSpatial().getLocalTranslation());                notifyAll();
             }
         }
    }
-
+    public void initialize(){
+        this.activatable = (IActivatable) this.getSpatial().getControl(ModelControl.class).getModel();
+    }
+    
     @Override
-    protected void controlUpdate(float f) {
-        //probably not needed
+    protected synchronized void controlUpdate(float f) {
+        if(this.activatable.isActivated()&&!hasSpawned){
+            this.hasSpawned = true;
+            this.activatable.inactivate();
+            if(this.activatable instanceof SpawnPoint){
+                NodeType type = ((SpawnPoint)this.activatable).getType();
+                int amount = ((SpawnPoint)this.activatable).getAmount();  
+                spawn(amount, type);
+            }
+        }
     }
 
     @Override
