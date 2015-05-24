881562f8646c1ae4b6f14db6cb3378fecacb343b
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-24 17:11:21 +0200

    Added proper collision width and height

--- src/main/java/edu/chl/LifeOfAGoblin/model/MeleeWeapon.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/MeleeWeapon.java
@@ -5,14 +5,24 @@
 package edu.chl.LifeOfAGoblin.model;
 
 /**
- *
+ * A class representing a melle weapon.
  * @author Ulrika
  */
 public class MeleeWeapon extends Weapon {
+    
+    /**
+     * Creates a default melee weapon.
+     */
     public MeleeWeapon() {
-        this(1, 20, 10);
+        this(1, 1, 1);
     }
     
+    /**
+     * Creates a melee weapon with a specified collision area and range.
+     * @param collisionHeight trhe height of the collision area
+     * @param collisionWidth the width of the collision area
+     * @param range the range of the weapon
+     */
     public MeleeWeapon(float collisionHeight, float collisionWidth, float range) {
         super(collisionHeight, collisionWidth, range);
     }
--- src/main/java/edu/chl/LifeOfAGoblin/model/RangedWeapon.java
+++ src/main/java/edu/chl/LifeOfAGoblin/model/RangedWeapon.java
@@ -5,15 +5,24 @@
 package edu.chl.LifeOfAGoblin.model;
 
 /**
- *
+ * A class representing a ranged weapon.
  * @author Ulrika
  */
 public class RangedWeapon extends Weapon {
     
+    /**
+     * Creates a default ranged weapon.
+     */
     public RangedWeapon() {
-        this(2, 20, 20);
+        this(2, 5, 5);
     }
     
+    /**
+     * Creates a ranged weapon with a specified collision area and range.
+     * @param collisionHeight the height of the collision area
+     * @param collisionWidth the width of the collision area
+     * @param range the range of the weapon
+     */
     public RangedWeapon(float collisionHeight, float collisionWidth, float range) {
         super(collisionHeight, collisionWidth, range);
     }
