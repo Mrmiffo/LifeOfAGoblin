dce740acb4dfd422c84a0243ff89a7afde7e37d4
Tandl-os <freb@student.chalmers.se>
2015-05-29 19:49:04 +0200

    bugfixes

--- src/test/java/edu/chl/LifeOfAGoblin/utils/PhysicsWrapperTest.java
+++ src/test/java/edu/chl/LifeOfAGoblin/utils/PhysicsWrapperTest.java
@@ -4,8 +4,11 @@
  */
 package edu.chl.LifeOfAGoblin.utils;
 
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.shapes.BoxCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.GhostControl;
 import com.jme3.bullet.control.RigidBodyControl;
@@ -15,6 +18,9 @@ import com.jme3.bullet.objects.PhysicsRigidBody;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.control.UpdateControl;
+import com.jme3.system.JmeSystem;
+import edu.chl.LifeOfAGoblin.jME3.game.LifeOfAGoblin;
+import java.util.Collection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -29,9 +35,6 @@ import static org.junit.Assert.*;
  * @author fredrik
  */
 public class PhysicsWrapperTest {
-        Node node1 = new Node();
-        Node node2 = new Node();
-        Node lvl = new Node();
         
     public PhysicsWrapperTest() {
 
@@ -40,6 +43,7 @@ public class PhysicsWrapperTest {
     
     @BeforeClass
     public static void setUpClass() {
+        
         
     }
     
@@ -54,16 +58,7 @@ public class PhysicsWrapperTest {
     
     @After
     public void tearDown() {
-        for(PhysicsGhostObject obj: ((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getGhostObjectList()){
-            PhysicsWrapper.getInstance().remove(obj);
-        }
-
-        for(PhysicsCharacter obj: ((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getCharacterList()){
-            PhysicsWrapper.getInstance().remove(obj);
-        }
-        for(PhysicsRigidBody obj: ((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getRigidBodyList()){
-            PhysicsWrapper.getInstance().remove(obj);
-        }        
+     
      }
 
     @Test
@@ -73,40 +68,7 @@ public class PhysicsWrapperTest {
     @Test
     
     public void testAdd_Control() {
-        //setup 
 
-        BoxCollisionShape shape = new BoxCollisionShape(new Vector3f(1,1,1));
-        GhostControl ghostControl = new GhostControl(shape);
-        RigidBodyControl rigidBodyControl = new RigidBodyControl(shape);
-        CharacterControl characterControl = new CharacterControl(shape, 1.0f);
-        UpdateControl updateControl = new UpdateControl();
-        
-        //tests that a Ghostcontrol is added
-        PhysicsWrapper.getInstance().add(ghostControl);
-        assertTrue(((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getGhostObjectList().contains(ghostControl));
-        
-        //tests that a Rigidbodycontrol can be added
-        PhysicsWrapper.getInstance().add(rigidBodyControl);
-        assertTrue(((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getRigidBodyList().contains(rigidBodyControl));
-        
-        //tests that a CharacterControl is added
-        PhysicsWrapper.getInstance().add(characterControl);
-        assertTrue(((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getCharacterList().contains(characterControl));
-        
-        //tests that an incomplete control is not added, exception will be 
-        //thrown if test fails
-        GhostControl faultyControl = new GhostControl();
-        PhysicsWrapper.getInstance().add(faultyControl);
-        assertFalse(((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getGhostObjectList().contains(faultyControl));
-        
-        //tests that a non PhysicsControl can't be added, exception will be 
-        //thrown if test fails
-//        PhysicsWrapper.getInstance().add(updateControl);
-        
-        //tests that the same control cannot be added twice
-        PhysicsWrapper.getInstance().add(ghostControl);
-        assertTrue(((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getGhostObjectList().size() == 1);
-        
         //tests that method can handle null input
         PhysicsWrapper.getInstance().add((GhostControl)null);
     }
@@ -114,21 +76,7 @@ public class PhysicsWrapperTest {
 
     @Test
     public void testRemove() {
-        //setup
-        GhostControl control = new GhostControl(new BoxCollisionShape(new Vector3f(1,1,1)));
-        
-        //tests that the object actually is removed
-        PhysicsWrapper.getInstance().add(control);
-        PhysicsWrapper.getInstance().remove(control);
-        assertTrue(((BulletAppState)StateManagerWrapper.getInstance().getAvailableState(BulletAppState.class)).getPhysicsSpace().getGhostObjectList().isEmpty());
-        
-        // tests that it can handle non PhysicsObect, should not throw exception. 
-//       PhysicsWrapper.getInstance().remove(12);
-    
-        // tests that it can handle removing objects that don't exist in the 
-        //physicsspace, should not throw exception.
-        PhysicsWrapper.getInstance().remove(control);
-        
+
         //tests that method can handle null input
         PhysicsWrapper.getInstance().remove(null);
     }
