bcd1fc60dbfdf250a83ce96c9b1ff1ed8e6ceac2
Tandl-os <freb@student.chalmers.se>
2015-05-29 15:03:49 +0200

    removed unused imports

--- src/test/java/edu/chl/LifeOfAGoblin/model/character/MinionTest.java
+++ src/test/java/edu/chl/LifeOfAGoblin/model/character/MinionTest.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.chl.LifeOfAGoblin.model;
+package edu.chl.LifeOfAGoblin.model.character;
 
-import edu.chl.LifeOfAGoblin.model.character.Minion;
+import edu.chl.LifeOfAGoblin.model.AbstractGameObject;
 import edu.chl.LifeOfAGoblin.model.NodeType;
 import org.junit.After;
 import org.junit.AfterClass;
