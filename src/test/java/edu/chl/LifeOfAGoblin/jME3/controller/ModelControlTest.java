03f4630fc99e1260d830c27873d6e44403550c3a
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-30 13:49:49 +0200

    Created partial ModelControl test

--- src/test/java/edu/chl/LifeOfAGoblin/jME3/controller/ModelControlTest.java
+++ src/test/java/edu/chl/LifeOfAGoblin/jME3/controller/ModelControlTest.java
@@ -4,6 +4,9 @@
  */
 package edu.chl.LifeOfAGoblin.jME3.controller;
 
+import edu.chl.LifeOfAGoblin.model.INode;
+import edu.chl.LifeOfAGoblin.model.character.Boss;
+import edu.chl.LifeOfAGoblin.model.character.Minion;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,6 +19,9 @@ import static org.junit.Assert.*;
  * @author Ulrika
  */
 public class ModelControlTest {
+    
+    private ModelControl mc;
+    private INode model;
     
     public ModelControlTest() {
     }
@@ -30,18 +36,31 @@ public class ModelControlTest {
     
     @Before
     public void setUp() {
+        model = new Minion();
+        mc = new ModelControl(model);
     }
     
     @After
     public void tearDown() {
+        model = null;
+        mc = null;
     }
 
     @Test
     public void testGetModel() {
+        //Tests that the model is returned correctly
+        assertTrue("ModelControlTest: Model is not of the correct class when using getModel()", mc.getModel().getClass() == Minion.class);
+        assertTrue("ModelControlTest: Model is not the correct object when using getModel()", mc.getModel() == model);
     }
 
     @Test
     public void testSetModel() {
+        INode newModel = new Boss();
+        mc.setModel(newModel);
+        
+        //Testing to see that the model is changed properly
+        assertFalse("ModelControlTest: Model is not changed correctly by setModel()", mc.getModel() == model);
+        assertTrue("ModelControlTest: Model is not changed correctly by setModel()", mc.getModel() == newModel);
     }
 
     @Test
