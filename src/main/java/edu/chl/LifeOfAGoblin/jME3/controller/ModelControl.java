7748ccc7dfd50df8a92e512c4074d531c45f6e7b
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-05-31 16:38:01 +0200

    Added javadoc

--- src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/ModelControl.java
+++ src/main/java/edu/chl/LifeOfAGoblin/jME3/controller/ModelControl.java
@@ -1,12 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.chl.LifeOfAGoblin.jME3.controller;
 
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
-import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 import edu.chl.LifeOfAGoblin.model.INode;
 
@@ -20,23 +15,37 @@ public class ModelControl extends AbstractControl {
 
     private INode model;
 
+    /**
+     * Creates an instance of ModelControl with a specified model.
+     * @param model the model
+     */
     public ModelControl(INode model) {
         this.model = model;
     }
 
+    /**
+     * Returns the model of the ModelControl.
+     * @return the model
+     */
     public INode getModel() {
         return model;
     }
 
+    /**
+     * Sets the model of the ModelControl.
+     * @param newModel the model to be handled by the ModelControl
+     */
     public void setModel(INode newModel) {
         model = newModel;
     }
 
     @Override
     protected void controlUpdate(float tpf) {
+        //Do nothing
     }
 
     @Override
     protected void controlRender(RenderManager rm, ViewPort vp) {
+        //Do nothing
     }
 }
